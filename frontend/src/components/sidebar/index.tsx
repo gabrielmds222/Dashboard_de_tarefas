bf3f16a54a8f3376a5f5aedca396b50ac36bd7e4
gabrielmedsilva <gabrielmedsilva@outlook.com>
2025-01-10 04:09:31 -0300

    feat: Sidebar global

--- frontend/src/components/sidebar/index.tsx
+++ frontend/src/components/sidebar/index.tsx
@@ -6,14 +6,111 @@ import {
   Package,
   PanelBottom,
   Settings2,
+  ShoppingBag,
+  Users,
 } from "lucide-react";
 import { Button } from "../ui/button";
 import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "../ui/sheet";
 import Link from "next/link";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
 
 export default function Sidebar() {
   return (
     <div className="flex w-full flex-col bg-muted/40">
+      <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 border-r bg-background sm:flex flex-col">
+        <nav className="flex flex-col items-center gap-4 px-2 py-5">
+          <TooltipProvider>
+            <Link
+              href="#"
+              className="flex h-9 w-9 shrink-0 items-center justify-center bg-primary text-primary-foreground rounded-full"
+            >
+              <Package className="w-4 h-4" />
+              <span className="sr-only">Dashboard Avatar</span>
+            </Link>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <Home className="w-5 h-5" />
+                  <span className="sr-only">Todas as tarefas</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent
+                className="bg-muted rounded-sm px-3 py-1 border-solid border-2"
+                side="right"
+              >
+                Todas as tarefas
+              </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <Check className="w-5 h-5" />
+                  <span className="sr-only">Concluídas</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent
+                className="bg-muted rounded-sm px-3 py-1 border-solid border-2"
+                side="right"
+              >
+                Concluídas
+              </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <Clock className="w-5 h-5" />
+                  <span className="sr-only">Pendentes</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent
+                className="bg-muted rounded-sm px-3 py-1 border-solid border-2"
+                side="right"
+              >
+                Pendentes
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </nav>
+        <nav className="mt-auto flex flex-col items-center gap-4 py-5">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <LogOut className="w-5 h-5 text-red-500" />
+                  <span className="sr-only">Sair</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent
+                className="bg-muted rounded-sm px-3 py-1 border-solid border-2"
+                side="right"
+              >
+                Sair
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </nav>
+      </aside>
+
       <div className="sm:hidden flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <header className="sticky top-0 z-30 flex h-14 items-center px-4 border-b bg-background gap-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
           <Sheet>
